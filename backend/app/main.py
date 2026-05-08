9058693d9fddb977290a6911ca5057e8d9e56e0a
ahmedomar <ahmedomar.dridi@esprit.tn>
2026-05-08 03:00:49 +0100

    fix: allow all origins in dev mode for CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,7 @@ app = FastAPI(title=settings.project_name)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.cors_origins,
+    allow_origins=settings.cors_origins if settings.environment == "prod" else ["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
